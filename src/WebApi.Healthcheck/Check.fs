ed4cbe009b450d82962e7c1ed8174a49009c09eb
Michał Niegrzybowski <michal.niegrzybowski@gmail.com>
2021-04-29 23:47:50 +0200

    Add WebApi healthcheck

---
+++ src/WebApi.Healthcheck/Check.fs
@@ -0,0 +1,25 @@
+﻿namespace WebApi.HealthCheck
+
+open System.Threading.Tasks
+open Microsoft.Extensions.Diagnostics.HealthChecks
+open WebRTC.Healthcheck.Model
+open WebRTC.Healthcheck.Utils
+open WebRTC.Healthcheck.Runner
+
+type WebRTCHealthCheck (toCheck: Request, log: Log) =
+    interface IHealthCheck with
+        member __.CheckHealthAsync (context, cancellationToken) =
+            let result = run log toCheck
+            let unhealthy =
+                result
+                |> Array.filter (fun x -> x.ResultCode <> 0)
+            let isSuccess = unhealthy |> Array.isEmpty
+            if isSuccess then
+                Task.FromResult (HealthCheckResult.Healthy("All WebRTC servers are healthy"))
+            else
+                let msg =
+                    unhealthy
+                    |> Array.map (fun x -> x.Address)
+                    |> String.concat ","
+                    |> fun x -> $"An unhealthy result for: %s{x} servers"
+                Task.FromResult (HealthCheckResult(context.Registration.FailureStatus, msg))
--- src/WebApi/Startup.fs
+++ src/WebApi/Startup.fs
@@ -1,19 +1,49 @@
 namespace WebApi
 
+open System
+open System.Threading.Tasks
+open HealthChecks.UI.Client
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Diagnostics.HealthChecks
 open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Diagnostics.HealthChecks
 open Microsoft.Extensions.Hosting
+open WebApi.HealthCheck
+open WebRTC.Healthcheck.Credentials
+open WebRTC.Healthcheck.Model
+open WebRTC.Healthcheck.Server
 
 type Startup(configuration: IConfiguration) =
+    let request = {
+        Servers = [| Address "serverhere" |]
+        CredentialsConfig = Some {
+            OTP = Otp "otphere"
+            UserPostfix = Some (UserPostfix "turn")
+        }
+    }
+    
     member _.Configuration = configuration
 
     // This method gets called by the runtime. Use this method to add services to the container.
     member _.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
+        services.AddHealthChecks()
+            .AddCheck(
+                "WebRTC",
+                WebRTCHealthCheck(
+                    request,
+                    {   Success = Console.WriteLine
+                        Failure = Console.WriteLine }
+                )
+            ) |> ignore
+        services
+            .AddHealthChecksUI(fun s -> s.AddHealthCheckEndpoint("WebRTC", "/health") |> ignore)
+            .AddInMemoryStorage() |> ignore
         services.AddControllers() |> ignore
-
+        
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member _.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
         if (env.IsDevelopment()) then
@@ -23,4 +53,12 @@ type Startup(configuration: IConfiguration) =
            .UseAuthorization()
            .UseEndpoints(fun endpoints ->
                 endpoints.MapControllers() |> ignore
+                endpoints.MapHealthChecksUI() |> ignore
+                endpoints.MapHealthChecks(
+                    "/health",
+                    HealthCheckOptions(
+                        Predicate = (fun _ -> true),
+                        ResponseWriter = Func<HttpContext, HealthReport, Task>(fun (context) (c: HealthReport) -> UIResponseWriter.WriteHealthCheckUIResponse(context, c))
+                    )
+                ) |> ignore
             ) |> ignore
